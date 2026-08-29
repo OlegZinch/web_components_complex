cc75c2d88b97f5ce376b8a3497ad9af8a3a2385e
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2026-08-29 20:35:41 +0100

    Public Methods & Properties

--- modal.js
+++ modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
+    this.isOpen = false
     this.shadowRoot.innerHTML = `
       <style>
         :host([opened]) #backdrop,
@@ -72,20 +73,25 @@ class Modal extends HTMLElement {
     `
   }
 
-  // attributeChangedCallback(name, oldValue, newValue) {
-  //   if (name === 'opened') {
-  //     if (this.hasAttribute('opened')) {
-  //       this.shadowRoot.querySelector('#backdrop').style.opacity = 1
-  //       this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all'
-  //       this.shadowRoot.querySelector('#modal').style.opacity = 1
-  //       this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all'
-  //     }
-  //   }
-  // }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.hasAttribute('opened')) {
+      this.isOpen = true
+      // this.shadowRoot.querySelector('#backdrop').style.opacity = 1
+      // this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all'
+      // this.shadowRoot.querySelector('#modal').style.opacity = 1
+      // this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all'
+    } else {
+      this.isOpen = false
+    }
+  }
 
-  // static get observedAttributes() {
-  //   return ['opened']
-  // }
+  static get observedAttributes() {
+    return ['opened']
+  }
+
+  open() {
+    this.setAttribute('opened', '')
+  }
 }
 
 customElements.define('my-modal', Modal)
